fe4f501bcd7878ba4005363771c939a380181541
Jinwoo Park (Curt) <www.jwpark.co.kr@gmail.com>
2017-12-24 21:36:55 +0900

    implemented VGGLike

--- vgg_like.py
+++ vgg_like.py
@@ -1,22 +1,23 @@
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
-from keras.layers import Input, Dense, Flatten
+from keras.layers import (Input, Conv2D, BatchNormalization,
+                          Activation, Dense, Dropout, Flatten)
 from keras.models import Model
 from model import BaseModel
 import utils
 import argparse
 
-MODEL_NAME = 'ExampleModel' # This should be modified when the model name changes.
+MODEL_NAME = 'VGGLike' # This should be modified when the model name changes.
 WEIGHTS_PATH = f'./models/{MODEL_NAME}.h5'
 IMAGE_PATH = f'./images/{MODEL_NAME}.png'
 PLOT_PATH  = f'./images/{MODEL_NAME}_plot.png'
 
-class ExampleModel(BaseModel):
+class VGGLike(BaseModel):
     '''
     A simple model with 2 FC layers.
     '_build()' is only modified when the model changes.
 
     HowToUse:
-        model = ExampleModel(PATH_FOR_WEIGHTS)
+        model = VGGLike(PATH_FOR_WEIGHTS)
         * all funtionalities are written in BaseModel.py
     '''
     def __init__(self, path_for_weights):
@@ -26,10 +27,40 @@ class ExampleModel(BaseModel):
 
     def _build(self):
         x = Input(shape = (28, 28, 1))
-        y = Flatten()(x)
-        y = Dense(units=64, activation='relu')(y)
-        y = Dense(units=10, activation='softmax')(y)
+        y = self._3conv_block(x, out_channel = 32, name = 'block1')
+        y = self._3conv_block(y, out_channel = 64, name = 'block2')
+        y = self._channel_reduction_conv(y, out_channel = 64, name = 'channel_reduction1')
+        y = self._channel_reduction_conv(y, out_channel = 32, name = 'channel_reduction2')
+        y = self._channel_reduction_conv(y, out_channel = 16, name = 'channel_reduction3')
+        y = self._channel_reduction_conv(y, out_channel =  8, name = 'channel_reduction4')
+        y = self._channel_reduction_conv(y, out_channel =  4, name = 'channel_reduction5')
+        y = Flatten()(y)
+        y = self._dense_relu_dropout(y, out_units = 64, name = 'dense1')
+        y = self._dense_relu_dropout(y, out_units = 32, name = 'dense2')
+        y = Dense(units = 10, activation='softmax', name = 'dense_softmax')(y)
         return Model(x, y, name = MODEL_NAME)
+
+    def _3conv_block(self, x, out_channel, name):
+        y = Conv2D(out_channel,     (3, 3), padding = 'same', name = f'{name}_conv1')(x)
+        y = Activation('relu')(BatchNormalization()(y))
+        y = Conv2D(out_channel,     (3, 3), padding = 'same', name = f'{name}_conv2')(y)
+        y = Activation('relu')(BatchNormalization()(y))
+        y = Conv2D(out_channel * 2, (2, 2), strides = (2,2), padding = 'valid',
+                                                             name = f'{name}_pooling_conv3')(y)
+        y = Activation('relu')(BatchNormalization()(y))
+        y = Dropout(rate = 0.1)(y)
+        return y
+
+    def _channel_reduction_conv(self, x, out_channel, name):
+        y = Conv2D(out_channel, (1, 1), padding = 'same', name = name)(x)
+        y = Activation('relu')(BatchNormalization()(y))
+        return y
+
+    def _dense_relu_dropout(self, x, out_units, name):
+        y = Dense(out_units, name = name)(x)
+        y = Activation('relu')(BatchNormalization()(y))
+        y = Dropout(rate = 0.2)(y)
+        return y
 
 def get_argument_parser():
     '''
@@ -84,7 +115,7 @@ def main():
     print(f'[data loaded]')
 
     # build and compile the model
-    model = ExampleModel(args.path_for_weights)
+    model = VGGLike(args.path_for_weights)
     model.compile()
     print('[model built]')
 
